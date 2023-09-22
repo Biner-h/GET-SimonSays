ad216725a640230526905ea432f82f03ae0306d4
Biner-h <Binerhabib@gmail.com>
2023-09-22 15:07:39 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //Model
 let app = document.getElementById("app");
+let colors = ["red", "green", "blue", "yellow"];
+let toggleColor = [];
+let userToggleColor = [];
+let score = 0;
 
 //View
 updateView();
@@ -14,3 +18,71 @@ function updateView() {
         </div>
     `;
 }
+
+//Controller
+function getRandomColor() {
+    randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+function addToToggleColor() {
+    toggleColor.push(getRandomColor());
+    playToggleColor();
+}
+
+function playToggleColor() {
+    let i = 0;
+    let interval = setInterval(() => {
+        blink(toggleColor[i]);
+        i++;
+        if (i >= toggleColor.length) {
+            clearInterval(interval);
+        }
+    }, 1000);
+    userToggleColor = [];
+}
+
+function blink(color) {
+    let square = document.getElementById(color);
+    square.style.opacity = 1;
+    setTimeout(() => {
+        square.style.opacity = 0.5;
+    }, 500);
+}
+
+function checkUserInput() {
+    for (let i = 0; i < userToggleColor.length; i++) {
+        if (userToggleColor[i] !== toggleColor[i]) {
+            alert("Feil! Spillet er over. Poengsum: " + score);
+            resetGame();
+            return;
+        }
+    }
+
+    if (userToggleColor.length === toggleColor.length) {
+        score++;
+        document.getElementById("score").textContent = score;
+        addToToggleColor();
+    }
+}
+
+function handleClick(event) {
+    let clickedColor = event.target.id;
+    userToggleColor.push(clickedColor);
+    blink(clickedColor);
+    checkUserInput();
+}
+
+function resetGame() {
+    toggleColor = [];
+    userToggleColor = [];
+    score = 0;
+    document.getElementById("score").textContent = score;
+    addToToggleColor();
+}
+
+document.querySelectorAll(".square").forEach((square) => {
+    square.addEventListener("click", handleClick);
+});
+
+resetGame();
